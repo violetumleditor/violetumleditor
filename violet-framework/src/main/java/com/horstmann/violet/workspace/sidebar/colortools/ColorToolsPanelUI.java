75871d1d5d4c20d3693fba50683a009c9454d52b
Alexandre de Pellegrin <alexandre.de.pellegrin@gmail.com>
2015-02-12 18:56:21 +0100

    working on color

--- violet-framework/src/main/java/com/horstmann/violet/workspace/sidebar/colortools/ColorToolsPanelUI.java
+++ violet-framework/src/main/java/com/horstmann/violet/workspace/sidebar/colortools/ColorToolsPanelUI.java
@@ -1,11 +1,14 @@
 package com.horstmann.violet.workspace.sidebar.colortools;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.awt.Graphics;
 
-import javax.swing.JButton;
+import javax.swing.Icon;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 import javax.swing.border.EmptyBorder;
 import javax.swing.plaf.PanelUI;
 
@@ -48,11 +51,36 @@ public class ColorToolsPanelUI extends PanelUI
             this.panel = new JPanel();
             this.panel.setOpaque(false);
             this.panel.setBorder(new EmptyBorder(0, 5, 0, 0));
-            GridLayout layout = new GridLayout(0, 5, 15, 10);
+            FlowLayout layout = new FlowLayout(FlowLayout.CENTER, 10, 10);
             this.panel.setLayout(layout);
         }
         return this.panel;
     }
+    
+    private JToggleButton getColorButton(Color color) {
+        JToggleButton button = new JToggleButton();
+        Icon icon = new Icon() {
+            @Override
+            public void paintIcon(Component c, Graphics g, int x, int y)
+            {
+                // SEE : http://swing124.rssing.com/browser.php?indx=20669962&item=17
+                
+            }
+            @Override
+            public int getIconWidth()
+            {
+                return 20;
+            }
+
+            @Override
+            public int getIconHeight()
+            {
+                return 20;
+            }
+        };
+        return button;
+    }
+    
 
     /**
      * Component(s panel
