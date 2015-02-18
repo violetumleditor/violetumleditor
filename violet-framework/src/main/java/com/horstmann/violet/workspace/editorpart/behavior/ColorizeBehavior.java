3f6141b8d0b405fb507c3afe3dfd1e7b8d811561
Alexandre de Pellegrin <alexandre.de.pellegrin@gmail.com>
2015-02-18 14:09:48 +0100

    working on color choice

--- violet-framework/src/main/java/com/horstmann/violet/workspace/editorpart/behavior/ColorizeBehavior.java
+++ violet-framework/src/main/java/com/horstmann/violet/workspace/editorpart/behavior/ColorizeBehavior.java
@@ -2,7 +2,11 @@ package com.horstmann.violet.workspace.editorpart.behavior;
 
 import java.awt.Cursor;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
 
+import com.horstmann.violet.product.diagram.abstracts.IColorable;
+import com.horstmann.violet.product.diagram.abstracts.edge.IEdge;
+import com.horstmann.violet.product.diagram.abstracts.node.INode;
 import com.horstmann.violet.workspace.IWorkspace;
 import com.horstmann.violet.workspace.sidebar.colortools.ColorChoice;
 import com.horstmann.violet.workspace.sidebar.colortools.IColorChoiceBar;
@@ -32,21 +36,38 @@ public class ColorizeBehavior extends AbstractEditorPartBehavior
     {
         if (event.getClickCount() > 1)
         {
+            this.workspace.getAWTComponent().setCursor(this.initialCursor);
             return;
         }
         if (event.getButton() != MouseEvent.BUTTON1)
         {
+            this.workspace.getAWTComponent().setCursor(this.initialCursor);
             return;
+        }
+        if (currentColorChoice == null)
+        {
+            this.workspace.getAWTComponent().setCursor(this.initialCursor);
+            return;
+        }
+        double zoom = this.workspace.getEditorPart().getZoomFactor();
+        Point2D mouseLocation = new Point2D.Double(event.getX() / zoom, event.getY() / zoom);
+        INode node = this.workspace.getGraphFile().getGraph().findNode(mouseLocation);
+        if (node != null && node.getClass().isAssignableFrom(IColorable.class))
+        {
+            IColorable colorableNode = (IColorable) node;
+            colorableNode.setBackgroundColor(this.currentColorChoice.getBackgroundColor());
+            colorableNode.setBorderColor(this.currentColorChoice.getBorderColor());
+            colorableNode.setTextColor(this.currentColorChoice.getTextColor());
         }
         this.currentColorChoice = null;
         this.workspace.getAWTComponent().setCursor(this.initialCursor);
     }
-    
-    
+
     @Override
     public void onMouseDragged(MouseEvent event)
     {
-        if (this.currentColorChoice == null) {
+        if (this.currentColorChoice == null)
+        {
             return;
         }
         this.workspace.getAWTComponent().setCursor(this.transitionCursor);
