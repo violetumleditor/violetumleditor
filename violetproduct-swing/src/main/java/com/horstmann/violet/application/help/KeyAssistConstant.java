4b990fb9d6f6650bccbb09dc3125171656e86cc0
Mateusz Mucha <khaleesy@o2.pl>
2016-02-10 13:32:40 +0100

    Key Assist
    Key assist with all available for use shorcuts in application of
    VioletUML along with some new shortcuts i.a. for new workspace of
    specific diagram.

---
+++ violetproduct-swing/src/main/java/com/horstmann/violet/application/help/KeyAssistConstant.java
@@ -0,0 +1,9 @@
+package com.horstmann.violet.application.help;
+/**
+ * Localized properties of available for use shortcut keys.
+ * @author Mateusz Mucha
+ *
+ */
+public interface KeyAssistConstant {
+	public static final String SHORTCUT_KEYS_STRINGS = "com.horstmann.violet.application.help._allKeysAssist_";
+}
