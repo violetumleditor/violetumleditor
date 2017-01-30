aed62d21c24a8a344b405fc22ec84d356141465f
alexdp <alexdp@free.fr>
2017-01-30 07:12:40 +0100

    working on node resize

--- violet-framework/src/main/java/com/horstmann/violet/product/diagram/abstracts/node/IResizableNode.java
+++ violet-framework/src/main/java/com/horstmann/violet/product/diagram/abstracts/node/IResizableNode.java
@@ -2,10 +2,24 @@ package com.horstmann.violet.product.diagram.abstracts.node;
 
 import java.awt.geom.Rectangle2D;
 
+/**
+ * Node size is automatic. However, by implementing this interface,
+ * the user isable to add extra width and extra height to this diagram element 
+ * 
+ * @author Alexandre de Pellegrin
+ *
+ */
 public interface IResizableNode
 {
-    void setWantedSize(Rectangle2D size);
+    void setExtraWidth(int width);
 
+    int getExtraWidth();
+
+    void setExtraHeight(int height);
+
+    int getExtraHeight();
+
+    
     /**
      * Methods returns point which allow to change size of node
      *
