319c2505b8c2dc452ebe475edcd1757ffbe32c9b
Alexandre DE PELLEGRIN <alexandre.depellegrin@ensea.fr>
2026-04-16 23:55:10 +0200

    fix: remove unused IOException import from CheerpJFileMenu

--- violetproduct-swing/src/main/java/com/horstmann/violet/application/cheerpj/CheerpJFileMenu.java
+++ violetproduct-swing/src/main/java/com/horstmann/violet/application/cheerpj/CheerpJFileMenu.java
@@ -4,7 +4,6 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.text.MessageFormat;
 import java.util.Comparator;
